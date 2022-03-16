720ff767ed11359d617c4d0f32c4b988fee962c0
sreeragtgcfbot <101707601+sreeragtgcfbot@users.noreply.github.com>
2022-03-17 03:18:32 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -39,7 +39,7 @@ async def clone_medias(client: Bot, message: Message):
     fn_caption = bool(query.file_caption)
     #
     if bool(clone_delay):
-        delay = 10
+        delay = 21600
     else:
         delay = 0.25
     #
